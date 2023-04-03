975137a44f1a0c5aaba63cb23bc169f71da465dc
Evan Yang <shancheng.yang@vaillant-group.com>
2023-04-03 21:36:05 +0800

    生命周期劫持-高级组件的特性

--- src/source/09-高阶组件/06-高阶组件-生命周期劫持.js
+++ src/source/09-高阶组件/06-高阶组件-生命周期劫持.js
@@ -1,9 +1,58 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent,Component } from 'react'
+
+
+function withRenderTime(WrappedComponent){
+  return class extends PureComponent{
+    componentDidMount(){
+      this.endTime = Date.now();
+      const interval = this.endTime - this.beginTime;
+      console.log(`${WrappedComponent.name}的渲染时间:${interval}`);
+    }
+
+    UNSAFE_componentWillMount(){
+      this.beginTime = Date.now();
+    }
+
+    render(){
+      return (
+        <WrappedComponent {...this.props}/>
+      )
+    }
+  }
+}
+
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        About
+      </div>
+    );
+  }
+}
+
+const HomeRenderTime = withRenderTime(Home);
+const AboutRenderTime = withRenderTime(About);
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <HomeRenderTime />
+        <AboutRenderTime />
+      </div>
     )
   }
 }
