287cce4b1e8ab854841ef038defd9c2b4789f0e1
Evan Yang <shancheng.yang@vaillant-group.com>
2023-05-06 17:11:16 +0800

    redux saga函数执行步骤

--- src/source/17-devtools-saga-reducer拆分/store/index.js
+++ src/source/17-devtools-saga-reducer拆分/store/index.js
@@ -3,6 +3,7 @@ import { createStore,applyMiddleware,compose } from 'redux';
 // 引入thunkMiddleware
 import thunkMiddleware from 'redux-thunk'
 // 创建一个createSagaMiddleware中间件，它是一个函数
+// 总结：步骤1-引入createSagaMiddleware这个函数
 import createSagaMiddleware from 'redux-saga'
 
 import mySaga from './saga.js';
@@ -14,14 +15,17 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace:true}
 // composeEnhancer.trace = true;
 
 // 创建其它的中间件
+// 总结：步骤2-创建createSagaMiddleware这个函数
 const sagaMiddleware = createSagaMiddleware();
 
 // 应用一些中间件，这个中间件可以让用户在actionCreators阶段，处理一些额外的情况，例如，请求网络，cookie解析，日志记录，文件压缩等操作
 // 这个中间件的目的，是在dispatch的action和我们最终到达的reducer之间，扩展自己的一些代码
 
 // 添加saga中间件
+// 总结：步骤3-为applyMiddleware添加多个中间件，这里就有2个中间件，thunkMiddleware和sagaMiddleware
  const storeEnhancer = applyMiddleware(thunkMiddleware,sagaMiddleware)
 
+//  总结：步骤4-storeEnhancer
 const store = createStore(reducer,composeEnhancer(storeEnhancer));
 
  //导入saga文件里的生成器函数,mysaga函数里定义了具体对哪些function进行拦截
--- src/source/17-devtools-saga-reducer拆分/store/saga.js
+++ src/source/17-devtools-saga-reducer拆分/store/saga.js
@@ -3,12 +3,17 @@ import { FETCH_HOME_MULTIDATA } from './constants';
 import { changeBannerAction,changeRecomendAction } from './actionCreators';
 import axios from 'axios';
 
+// 总结：步骤5-定义一个生成器函数mySaga，其中的一个action为FETCH_HOME_MULTIDATA,第二个参数是执行的fetchHomeMultiData生成器函数
 // 定义生成器函数,生成器函数是在action和reducer之间做一些action的额外操作
 function *mySaga(){
   // 执行一个生成器函数,对FETCH_HOME_MULTIDATA这个action进行拦截,拦截后并执行生成器函数
+  // takeEvery 和 takeLatest的区别
+  // takeLatest:依次只能监听一个action
+  // takeEvery:每一个都会执行
   yield takeEvery(FETCH_HOME_MULTIDATA,fetchHomeMultiData)
 }
 
+// 总结：步骤6-定义一个生成器函数fetchHomeMultiData生成器函数，yield all定义一个数组，执行所有的生成器函数
 function *fetchHomeMultiData(){
   const result = yield axios({
     url:'https://mockapi.eolink.com/U6CZxWM9cedf95a49b4284e3e1a7985c53f89a9ccda5d45/home/multidata?responseId=1233072',
@@ -23,6 +28,7 @@ function *fetchHomeMultiData(){
   // yield put(changeBannerAction(banners))
 
   // 写法2
+  // 总结：步骤7-yield all定义一个数组,执行所有的生成器函数
   yield all([
     yield put(changeRecomendAction(recommends)), 
     yield put(changeBannerAction(banners))
