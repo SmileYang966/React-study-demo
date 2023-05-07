f7993a8866f94a574ac644d8cc3e3e1090e7e133
Evan Yang <shancheng.yang@vaillant-group.com>
2023-05-07 21:07:12 +0800

    reducer拆封讲解

--- src/source/18-中间件原理/store/reducer.js
+++ src/source/18-中间件原理/store/reducer.js
@@ -1,14 +1,19 @@
 import { ADD_NUMBER, SUB_NUMBER,INCREASEMENT,DECREASEMENT,CHANGE_BANNERS,CHANGE_RECOMMENDS } from './constants.js'
 
-const initData = {
-  counter : 0,
-  banners : [],
-  recommends : []
-}
+
 
 // reducer主要是定义了各个action的实现
-const reducer = (state = initData,action) => {
-  console.log("state is ",state,action)
+const reducer = (state = {},action) => {
+  return {
+    counterInfo : counterReducer(state.counterInfo,action),
+    homeInfo : homeReducer(state.homeInfo,action)
+  }
+}
+
+const counterInitData = {
+  counter : 0
+}
+const counterReducer = (state = counterInitData,action) => {
   switch(action.type){
     case INCREASEMENT:
       return { ...state, counter : state.counter + 1 }
@@ -17,7 +22,18 @@ const reducer = (state = initData,action) => {
     case ADD_NUMBER:
       return { ...state, counter : state.counter + action.num }
     case SUB_NUMBER:
-      return { ...state, counter : state.counter - action.num }
+      return { ...state, counter : state.counter - action.num }    
+    default:
+      return state; 
+  }
+}
+
+const homeInitData = {
+  banners : [],
+  recommends : []
+}
+const homeReducer = (state = homeInitData,action) => {
+  switch(action.type){
     case CHANGE_BANNERS:
       return { ...state, banners : action.banners }
     case CHANGE_RECOMMENDS:
