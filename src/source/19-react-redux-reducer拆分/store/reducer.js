e6791cfa74aa8377465b988381b9974aa873641c
Evan Yang <shancheng.yang@vaillant-group.com>
2023-05-08 22:41:57 +0800

    利用combineReducers来组合多个reducer

--- src/source/19-react-redux-reducer拆分/store/reducer.js
+++ src/source/19-react-redux-reducer拆分/store/reducer.js
@@ -1,12 +1,21 @@
 import { reducer as homeReducer } from './home'
 import { reducer as counterReducer } from './counter'
 
+import { combineReducers } from 'redux'
+
 // reducer主要是定义了各个action的实现
-const reducer = (state = {},action) => {
-  return {
-    counterInfo : counterReducer(state.counterInfo,action),
-    homeInfo : homeReducer(state.homeInfo,action)
-  }
-}
+// const reducer = (state = {},action) => {
+//   return {
+//     counterInfo : counterReducer(state.counterInfo,action),
+//     homeInfo : homeReducer(state.homeInfo,action)
+//   }
+// }
+
+// 使用redux提供的combineReducers，对counterReducer和homeReducer进行合并
+const reducer = combineReducers({
+  counterInfo : counterReducer,
+  homeInfo : homeReducer
+})
+
 
 export default reducer;
