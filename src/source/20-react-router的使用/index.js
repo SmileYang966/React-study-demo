ce39e80a95a2a15a08ffdd1b873128777b5ed370
Evan Yang <shancheng.yang@vaillant-group.com>
2023-05-14 09:51:11 +0800

    为active的item添加样式

--- src/source/20-react-router的使用/index.js
+++ src/source/20-react-router的使用/index.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react'
 import About from './pages/About'
 import Home from './pages/Home'
 import Mine from './pages/Mine'
-import { BrowserRouter, Route, Link, HashRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Link, HashRouter, Routes, NavLink } from 'react-router-dom'
+import './App.css'
 
 export default class index extends PureComponent {
   render() {
@@ -10,9 +11,31 @@ export default class index extends PureComponent {
       <div>
         <BrowserRouter>
         {/* Link都会渲染成a标签 */}
-          <Link to="/">首页</Link>
+          {/* <Link to="/">首页</Link>
           <Link to="/About">关于</Link>
-          <Link to="/Mine">我的</Link>
+          <Link to="/Mine">我的</Link> */}
+
+          {/* active的方式1 */}
+          {/* <NavLink to="/" style={({isActive, isPending}) => {
+            return {
+              color : isActive ? 'red' : isPending ? 'blue' : 'black'
+            }
+          }}>首页</NavLink>
+          <NavLink to="/About" style={({isActive, isPending}) => {
+            return {
+              color : isActive ? 'red' : isPending ? 'blue' : 'black'
+            }
+          }}>关于</NavLink>
+          <NavLink to="/Mine" style={({isActive, isPending}) => {
+            return {
+              color : isActive ? 'red' : isPending ? 'blue' : 'black'
+            }
+          }}>我的</NavLink> */}
+
+          {/* active的方式2 */}
+          <NavLink to="/">首页</NavLink>
+          <NavLink to="/About">关于</NavLink>
+          <NavLink to="/Mine">我的</NavLink>
 
           <Routes>
             <Route exact path="/" Component={Home}/>
